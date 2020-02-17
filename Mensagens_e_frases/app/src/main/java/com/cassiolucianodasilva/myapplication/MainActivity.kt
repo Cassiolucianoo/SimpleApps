a7d558e8e1c1be77efebe807bbff8c183783d96b
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2020-02-17 16:31:27 -0300

    pleading
    Elements SplashScreen, interface -
    MainActivity - ActionBar

--- Mensagens_e_frases/app/src/main/java/com/cassiolucianodasilva/myapplication/MainActivity.kt
+++ Mensagens_e_frases/app/src/main/java/com/cassiolucianodasilva/myapplication/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mensagensefrases
+package com.cassiolucianodasilva.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- Mensagens_e_frases/app/src/test/java/com/cassiolucianodasilva/myapplication/ExampleUnitTest.kt
+++ Mensagens_e_frases/app/src/test/java/com/cassiolucianodasilva/myapplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.mensagensefrases
+package com.cassiolucianodasilva.myapplication
 
 import org.junit.Test
 
